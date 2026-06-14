23f307da2cbce5a6c8bdabc546e7eaf18426b476
Chester Chan <chesterchan@Chesters-MacBook-Pro.local>
2026-06-14 17:30:56 +0100

    Keep only proof of concept 3 notebook

--- pipeline/data_loading.py
+++
@@ -1,9 +0,0 @@
-from qptifffile import QPTiffFile
-import matplotlib.pyplot as plt
-import numpy as np
-
-def image_loader(file):
-    f = QPTiffFile(file)
-    opal780_image = f.read_region('Opal 780')
-    return opal780_image
-
